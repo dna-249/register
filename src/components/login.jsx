00a22711766c726f645c7249e722dde4456b8740
nuraalhaji <danamonuraa@gmail.com>
2025-03-21 11:42:04 +0100

    commit

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -13,15 +13,7 @@ const Login = () => {
     }).then(res => {alert("your token is available" + res.data.token);
       handleVerify(res.data.token)}).catch(err => console.log(err))
   }
-  const handleVerify = (token) => {
-    axios.post("/https://register-api2.vercel.app/staff/verify",{
-      name:user,
-      password:password,
-      headers:{
-        Authorization:token
-      }
-    }).then(res =>{console.log(res);alert(res.data)})
-  }
+ 
   
 
   return (
