ad5f45e25bed62b851857acb20233b0aca2b37f0
nuraalhaji <danamonuraa@gmail.com>
2025-03-22 12:14:50 +0100

    commit

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
    
   }
   useEffect(() => {
-  if(typeof users !== "undefined") {handleLogin();}
+  if(typeof users !== "undefined" && users !== "") {handleLogin();}
   }, [users])
 
   useEffect(() => {
