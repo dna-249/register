1c48512b77d03cd4c62c420a0e2a812faa8f13f9
nuraalhaji <danamonuraa@gmail.com>
2025-03-24 19:46:07 +0100

    commit

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -31,7 +31,7 @@ useEffect(() => {
 
 
  const handleRegister = async(add)=>{
-       await axios.put(`https://register-api-cloud.vercel.app/student/push/${add}`,{
+       await axios.put(`https://register-api-cloud.vercel.app/student/student/push/${add}`,{
         date:Date().slice(0,21),
         mon:'n',
         tue:'n',
