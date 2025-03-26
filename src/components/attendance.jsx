1610fd82af9f14c9172cd11c5feec8b803230c7d
nuraalhaji <danamonuraa@gmail.com>
2025-03-26 18:21:57 +0100

    commit

--- src/components/attendance.jsx
+++ src/components/attendance.jsx
@@ -21,7 +21,7 @@ const Attendance =({datas , handleRegister})=>{
     
     
     const handleSave = async()=>{
-           await axios.put(`https://register-api-cloud.vercel.app/student/${select2}`,{
+           await axios.put(`https://register-api-cloud.vercel.app/student/${data._id}`,{
             tue:setSelect3
            })
                      .then((res)=> console.log(res.data))
