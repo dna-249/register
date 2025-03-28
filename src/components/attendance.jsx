83d031065c1944cdee9cf8efe13c51bb9fdc5f57
nuraalhaji <danamonuraa@gmail.com>
2025-03-28 12:06:05 +0100

    commit

--- src/components/attendance.jsx
+++ src/components/attendance.jsx
@@ -20,7 +20,7 @@ const Attendance =({datas})=>{
     }
 
     const handleRegister = async(add)=>{
-           await axios.put(`https://localhost:3000/student/push/${add}`,{
+           await axios.put(`http://localhost:3000/student/push/${add}`,{
             date:Date().slice(0,21),
             mon:'',
             tue:'',
