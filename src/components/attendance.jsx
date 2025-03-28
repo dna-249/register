8fca34482da66cffd72f2a57eb699634a3303224
nuraalhaji <danamonuraa@gmail.com>
2025-03-28 11:49:25 +0100

    commit

--- src/components/attendance.jsx
+++ src/components/attendance.jsx
@@ -20,7 +20,7 @@ const Attendance =({datas})=>{
     }
 
     const handleRegister = async(add)=>{
-           await axios.put(`https://register-api-cloud.vercel.app/student/push/${add}`,{
+           await axios.put(`https://localhost:3000/student/push/${add}`,{
             date:Date().slice(0,21),
             mon:'',
             tue:'',
