5ee82fc8bb49c662b32776c39c473b37f738c72e
nuraalhaji <danamonuraa@gmail.com>
2025-04-01 23:06:47 +0100

    commit

--- src/components/staff.jsx
+++ src/components/staff.jsx
@@ -37,7 +37,7 @@ const Staff = () => {
     </div>
     <div>
          <h2> Student List </h2>
-         {names.map((name,index) => <div key={index}>
+         {names?.map((name,index) => <div key={index}>
           {name.name}
          </div>)}
 
