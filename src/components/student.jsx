f055e2cc83b9e7beb627be3050b3fb9609f1a38d
nuraalhaji <danamonuraa@gmail.com>
2025-04-02 21:34:32 +0100

    commit

--- src/components/student.jsx
+++ src/components/student.jsx
@@ -102,7 +102,7 @@ const Student = ({name}) => {
      Term:
      
      
-     <div className='white'>{isNew?(
+     <div className='white'>
        
          <div>
            <div style={{display:"grid",justifyContent:"flex-end"}} >
@@ -111,41 +111,8 @@ const Student = ({name}) => {
      <h2>Student Attendance</h2>
            
                               <Attendance datas ={name} setIsNews ={setIsNew} />
-           </div>):isUpdate?(
-         <div style={{padding:"5px"}}>
-           <div style={{width:"fit-content",}} className='three'>
-     <div  className='click2'  onClick={()=>setIsNew((pre)=>!pre)}> Attendance</div>
-     <div className='click2' onClick={()=>setIsUpdate((pre)=>!pre)}>Update </div>
-     <div  className='click2'  onClick={()=>setIsNew(false)}>New </div>
-     </div>
-           <h2>
-             {data === false && <div>Nuraalhaji Present</div>}{data === true && <div className='red'>Nuralhaji Absent</div>} 
-             <br />{Date().slice(0,21)}</h2>
-     <label htmlFor=""> 
-         Mon
-         <input checked={day === "Mon"} onChange={(e)=>setDay(e.target.value)} value="Mon"  type='radio' />
-         Tue
-         <input checked={day === "Tue"} onChange={(e)=>setDay(e.target.value)} value="Tue" type='radio' />
-         Wed
-         <input checked={day === "Wed"} onChange={(e)=>setDay(e.target.value)} value="Wed" type='radio' />
-         Thu
-         <input checked={day === "Thu"} onChange={(e)=>setDay(e.target.value)} value="Thu" type='radio' />
-         Fri
-         <input checked={day === "Fri"} onChange={(e)=>setDay(e.target.value)} value="Fri" type='radio' />
-        
-     </label>
-    <div className='two'>
-     <div style={{justifySelf:"flex-end",display:"grid",marginRight:"40px"}}>Present</div>
-     <div> Absent</div>
-    </div>
-     <div className='two '>
-         <div className='click' onClick={()=>setData(pre => !pre)}>{data? (<div></div>):(<div className='green2'><FaCheck/></div>)}</div>
-         <div style={{justifySelf:"flex-start"}} className='click' onClick={()=>setData(pre => !pre)}>{data? (<div className='red2'><FaTimes/></div>):(<div></div>)}</div>
-          </div> <div style={{width :"200px",justifySelf:"center" }} className='click save'  onClick={()=>name()}>Save Attendance</div>
-          </div>):( 
-          <div>
-                 <Update datas={name}   setIsNews ={setIsNew}/>
-          </div>  )}
+           </div>
+           
      </div>
  </div>
    </div></>
