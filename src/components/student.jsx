562dc2876e6230dd8d8e48f09203e9ef2a896fbe
nuraalhaji <danamonuraa@gmail.com>
2025-04-07 23:33:40 +0100

    commit

--- src/components/student.jsx
+++ src/components/student.jsx
@@ -37,7 +37,7 @@ const Student = () => {
  </div>
    <div className='two'>
   
-   <div> <h4>{name.name}</h4> </div> 
+   <div> <h4>{name.name.user}</h4> </div> 
    <div><span style={{color:"blue", textDecoration:"underline"}}></span></div>
  
    </div>
@@ -90,7 +90,7 @@ const Student = () => {
  <div className='bgUser'> 
      <h3>REGISTER</h3>
      <div ><FaUser className='img'/></div>
-     name: {name.name}<br />
+     name: {name.name.user}<br />
      class: <br />
      Term:
      
