150e78544e987932fd18f857b8a2454037458caa
nuraalhaji <danamonuraa@gmail.com>
2025-04-08 12:35:19 +0100

    commit

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaBars, FaUser ,FaCheck, FaArrowCircleLeft, FaTimes} from 'react-icons/fa'
 import { Link ,useParams} from 'react-router-dom'
 import Attendance from "./attendance"
