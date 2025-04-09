3276cf268ab88b8cb1e275d960aeb0c197413095
nuraalhaji <danamonuraa@gmail.com>
2025-04-09 22:56:31 +0100

    commit

--- src/components/update.jsx
+++ src/components/update.jsx
@@ -58,43 +58,6 @@ const Update =()=>{
      
      
      <div className='white'> 
-         <div style={{padding:"5px"}}>
-           <div style={{width:"fit-content",}} className='three'>
-     <div  className='click2'  onClick={()=>setIsNew((pre)=>!pre)}> Attendance</div>
-     <div className='click2' onClick={()=>setIsUpdate((pre)=>!pre)}>Update </div>
-     <div  className='click2'  onClick={()=>setChange(()=> change + 1)}>New </div>
-     </div>
-           <h2>
-             {data === false && <div>{name?.name} Present</div>}{data === true && <div className='red'>{name?.name} Absent</div>} 
-             <br />{Date().slice(0,21)}</h2>
-     <label htmlFor=""> 
-         Mon
-         <input checked={day === "Mon"} onChange={(e)=>setDay(e.target.value)} value="Mon"  type='radio' />
-         Tue
-         <input checked={day === "Tue"} onChange={(e)=>setDay(e.target.value)} value="Tue" type='radio' />
-         Wed
-         <input checked={day === "Wed"} onChange={(e)=>setDay(e.target.value)} value="Wed" type='radio' />
-         Thu
-         <input checked={day === "Thu"} onChange={(e)=>setDay(e.target.value)} value="Thu" type='radio' />
-         Fri
-         <input checked={day === "Fri"} onChange={(e)=>setDay(e.target.value)} value="Fri" type='radio' />
-        
-     </label>
-    <div className='two'>
-     <div style={{justifySelf:"flex-end",display:"grid",marginRight:"40px"}}>Present</div>
-     <div> Absent</div>
-    </div>
-     <div className='two '>
-         <div className='click' onClick={()=>setData(pre => !pre)}>{data? (<div></div>):(<div className='green2'><FaCheck/></div>)}</div>
-         <div style={{justifySelf:"flex-start"}} className='click' onClick={()=>setData(pre => !pre)}>{data? (<div className='red2'><FaTimes/></div>):(<div></div>)}</div>
-          </div> <div style={{width :"200px",justifySelf:"center" }} className='click save'  onClick={()=>name()}>Save Attendance</div>
-          </div>
-     </div>
-     
- 
-
-
-        <div>
           <div style={{display:"grid",justifyContent:"flex-end"}}>
   </div>
   <h3>Student Attendance Updating</h3>
