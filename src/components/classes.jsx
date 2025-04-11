d44963cd7993c1e29cd119fbb72cd463cf9e8f50
nuraalhaji <danamonuraa@gmail.com>
2025-04-11 11:05:59 +0100

    commit

--- src/components/classes.jsx
+++ src/components/classes.jsx
@@ -1,7 +1,21 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 const Classes = ({id}) => {
   const [adm,setAdm]= useState('')
+
+
+ useEffect(() => {
+ 
+    axios.get(`https://register-api-cloud.vercel.app/management/${id}`)
+              .then((res)=> {console.log(res.data);setNames(res.data)})
+              .catch((err)=> console.log(err))
+  
+  
+ }, [])
+ 
+ 
+
+
   const handleCreate = (params) => {
     axios.put(`https://register-api-cloud.vercel.app/management/push/${id}/admissions/adm`,{adm:adm})
                 .then((res)=> alert(`${adm} admission is registered`))
@@ -15,14 +29,17 @@ const Classes = ({id}) => {
             <div className='white'>
                  <h4>Classes</h4>
                  <div>
-                    <h5>Asign Class</h5>
+                    <h5>Assign Class</h5>
                 
                     <input type="text" onChange={(e)=>setAdm(e.target.value)} placeholder='new admission no...'/>
-                    <button onClick={()=>handleCreate()}>Asign</button>
+                    <button onClick={()=>handleCreate()}>Assign</button>
                  </div>
 
                  <div>
-                   <h5>Previous Classes</h5> 
+                   <h5>Previous Classes</h5>
+                   {names?.classes.map((name,index)=>{return(
+                    <div key={index}>{name}</div>
+                   )})} 
                  </div>
             </div>
     
