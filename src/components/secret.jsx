fee28d0cdc3eb979f4284d956c1e7996ede61db7
nuraalhaji <danamonuraa@gmail.com>
2025-04-11 14:52:35 +0100

    commit

--- src/components/secret.jsx
+++ src/components/secret.jsx
@@ -11,7 +11,7 @@ const Secret = ({id}) => {
    useEffect(() => {
    
       axios.get(`https://register-api-cloud.vercel.app/management/${id}`)
-               .then((res)=> {console.log(res.data);setName(res.data)})
+               .then((res)=> {alert(res.data);setName(res.data)})
                .catch((err)=> console.log(err))
    
    
