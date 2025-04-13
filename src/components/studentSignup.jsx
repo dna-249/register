0973747c35f7a3e030f094ccaf676673931bde90
nuraalhaji <danamonuraa@gmail.com>
2025-04-13 15:11:57 +0100

    commit

--- src/components/studentSignup.jsx
+++ src/components/studentSignup.jsx
@@ -17,7 +17,7 @@ const StudentSignup = ({setLogin}) => {
   
 
   const handleSignUp =()=>{
-    axios.post("https://register-api2.vercel.app/student/admissions/key",
+    axios.post("https://register-api2.vercel.app/student",
       { key:adm,
         name:name,
         user:user,
