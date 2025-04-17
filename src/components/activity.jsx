5b309fdb73028bda127955276e756ca70cadeb6b
nuraalhaji <danamonuraa@gmail.com>
2025-04-17 12:39:42 +0100

    commit

--- src/components/activity.jsx
+++ src/components/activity.jsx
@@ -125,13 +125,13 @@ const test =[{questions :["what is you name?",
         <div  key={index} > 
         <h1>{item.question}</h1>          
         <h3>
-              A - {item.a} <input checked={select === 'A'} value='A'  type='radio' onChange={(e)=>setSelect(e.target.value)}/>
+              A - {item.a} <input checked={select === 'A'} value='A'  type='radio' onChange={(e)=>setSelect(e.target.value)}/> <br />
             
-              B  - {item.b}<input checked={select === 'B'} value='B' type='radio' onChange={(e)=>setSelect(e.target.value)}/>
+              B  - {item.b}<input checked={select === 'B'} value='B' type='radio' onChange={(e)=>setSelect(e.target.value)}/><br />
             
-              C - {item.c} <input checked={select === 'C'} value='C' type='radio' onChange={(e)=>setSelect(e.target.value)}/>
+              C - {item.c} <input checked={select === 'C'} value='C' type='radio' onChange={(e)=>setSelect(e.target.value)}/><br />
             
-              D - {item.d} <input checked={select === 'D'} value='D' type='radio' onChange={(e)=>setSelect(e.target.value)}/>
+              D - {item.d} <input checked={select === 'D'} value='D' type='radio' onChange={(e)=>setSelect(e.target.value)}/><br />
               </h3>
             
             <div  onClick={()=>name(item.ans[count])}><h1>Next</h1></div></div>)
