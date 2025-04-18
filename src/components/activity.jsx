de8d2cd9cb685944c8d3288affbbf4b9807d98b0
nuraalhaji <danamonuraa@gmail.com>
2025-04-18 11:50:21 +0100

    commit

--- src/components/activity.jsx
+++ src/components/activity.jsx
@@ -141,7 +141,7 @@ const test =[{questions :["what is you name?",
   
     {staff?.[`${object}`]
     .filter((item,index) =>{ 
-      return item.session === session || item.term === term || index === count
+      return item.session === session 
       }).map((item, index)=>
         { const setting = ()=>{ return item.ans};
            const answer = setting();  
