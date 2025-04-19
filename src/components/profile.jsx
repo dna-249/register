29ee49cbece8f85417c201fc4da77f9b54fa559d
nuraalhaji <danamonuraa@gmail.com>
2025-04-19 16:47:29 +0100

    commit

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -29,7 +29,7 @@ const Profile = () => {
                     .catch((err)=> console.log(err))
                     
    axios.put(`https://register-api-cloud.vercel.app/student/${id}`,
-    {image:`https://myshop-image-api.up.railway.app/file/${image1?.name}`})
+    {image:`https://imageapi-production-c98c.up.railway.app/file/${image1?.name}`})
                                     .then((res)=> {console.log(res.data);alert("image uploaded successfully 02")})
                                     .catch((err)=> console.log(err))   
   }
