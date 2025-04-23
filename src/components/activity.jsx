8e086d811057b98bf03dddb8b77004ccc9efb848
nuraalhaji <danamonuraa@gmail.com>
2025-04-23 12:10:19 +0100

    commit

--- src/components/activity.jsx
+++ src/components/activity.jsx
@@ -4,6 +4,7 @@ import { useState,useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useParams, useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import Timer from './timer'
 
 
 const Activity = ({names,setBack}) => {
