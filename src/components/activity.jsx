dc35008a4e6bc97ed19b0fe87768c60ea8293060
nuraalhaji <danamonuraa@gmail.com>
2025-04-28 22:12:29 +0100

    commit

--- src/components/activity.jsx
+++ src/components/activity.jsx
@@ -16,7 +16,7 @@ const Activity = ({setBack}) => {
     const [image,setImage]=useState('')
     const [image1,setImage1]=useState('')
     const [names,setNames]=useState('')
-    const [timeOut,setTimeOut]= useState(true)
+    const [timeOut,setTimeOut]= useState('')
     const [object, setObject]= useState("Eng")
     const [session, setSession]= useState("2024/2025")
     const [term, setTerm]= useState("first")
@@ -168,7 +168,7 @@ const Activity = ({setBack}) => {
 
 
 <div className='white'>
-  {duration >= 0  ?  <div> 
+  {duration >= 0 || count > staff?.[`${staff?.type}`].length   ?  <div> 
   
     {staff?.[`${staff?.type}`].filter((item,index) =>{ 
       return item.session === staff?.session && item.term === staff?.term && index === count 
