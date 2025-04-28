b6e4fe3cbd431e6a74147ab16e1505489f45d2a9
nuraalhaji <danamonuraa@gmail.com>
2025-04-28 22:05:01 +0100

    commit

--- src/components/activity.jsx
+++ src/components/activity.jsx
@@ -168,7 +168,7 @@ const Activity = ({setBack}) => {
 
 
 <div className='white'>
-  {duration === 0  ?  <div> 
+  {duration =< 0  ?  <div> 
   
     {staff?.[`${staff?.type}`].filter((item,index) =>{ 
       return item.session === staff?.session && item.term === staff?.term && index === count 
