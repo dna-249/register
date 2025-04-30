f24b56e90c4d5aa89267e2b50fb39c3a138c42c7
nuraalhaji <danamonuraa@gmail.com>
2025-04-30 01:22:04 +0100

    commit

--- src/components/student.jsx
+++ src/components/student.jsx
@@ -94,8 +94,8 @@ useEffect(() => {
     </thead>
      </div>
      {staff?.filter((item)=> {return item.class === name?.class})
-   .map((item,index)=>{const unique =()=>{ setTime(()=>item._id)}
-               const times = unique()
+   .map((item,index)=>{const unique =()=>{return setTime(()=>item._id)}
+               const times = unique();
                return (<div key={index}>{times}</div>)
    })}
      <div className='row5 green'>
