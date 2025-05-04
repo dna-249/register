0391f2106166e884c85d5c53653b81093778b415
nuraalhaji <danamonuraa@gmail.com>
2025-05-04 23:22:25 +0100

    commit

--- src/components/studentSignup.jsx
+++ src/components/studentSignup.jsx
@@ -17,7 +17,7 @@ const StudentSignup = ({setLogin}) => {
   
 
   const handleSignUp =()=>{
-    if(adm !== ''){
+   
     axios.post("https://database-api-eight.vercel.app/student",
       { key:adm,
         name:name,
@@ -28,9 +28,7 @@ const StudentSignup = ({setLogin}) => {
       })
       .then((res)=>alert("successfully created"))
       .catch((err)=> {alert("invalid Admission Number or network problem");console.log(err)})
-    }else{
-      alert("input field is required")
-    }
+    
   }
   
   return (
@@ -39,7 +37,7 @@ const StudentSignup = ({setLogin}) => {
     <div style={{borderRadius:"10px",padding:"20px",border:" 1px solid rgba(128, 127, 127, 0.28)"}}>
     <div><FaUser className='img'/> <br />
      <span style={{color:"green",fontWeight:"bolder"}}>Student Sign Up</span>  </div>
-        <div>Admission No:<input onChange={(e)=>setAdm(e.target.value)}    type='text'/></div>
+        <div>Admission No:<input onChange={(e)=>setAdm(e.target.value)}   required  type='text'/></div>
         <div>Name:<input onChange={(e)=>setName(e.target.value)}     type='text'/></div>
         <div>Create Username:<input onChange={(e)=>setUser(e.target.value)}    type='text'/></div>
         <div>Create Password:<input onChange={(e)=>setPassword(e.target.value)}    type='password'/></div>
