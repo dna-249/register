44f85ff7ba6949692704b5d48408046c3951a645
nuraalhaji <danamonuraa@gmail.com>
2025-07-19 09:13:39 +0100

    commit

--- src/components/classes.jsx
+++ src/components/classes.jsx
@@ -9,6 +9,7 @@ const Classes = ({id}) => {
   const [name,setName]= useState([''])
   const [name1,setName1]= useState([''])
   const [name2,setName2]= useState([''])
+  const [name3,setName3]= useState([''])
   const [auto,setAuto]= useState(false)
 
 
@@ -24,6 +25,10 @@ const Classes = ({id}) => {
 
     axios.get(`https://database-api-eight.vercel.app/student`)
               .then((res)=> {console.log(res.data);setName2(res.data)})
+              .catch((err)=> console.log(err))
+  
+              axios.get(`https://database-api-eight.vercel.app/teacher`)
+              .then((res)=> {console.log(res.data);setName3(res.data)})
               .catch((err)=> console.log(err))
   
  }, [id, auto])
@@ -53,6 +58,18 @@ const Classes = ({id}) => {
                   .catch((err)=> console.log(err))
     
     }
+const handleAssign3 = (name) => {
+      axios.put(`https://database-api-eight.vercel.app/teacher/push/${select?._id}/class/class`,{classes:name})
+                .then((res)=> {alert(`${select?.name}  is assigned class ${name}`);setAuto((prev)=>!prev);setShow(true)})
+                  .catch((err)=> console.log(err))
+    
+    }
+    const handleAssign4 = (name) => {
+      axios.put(`https://database-api-eight.vercel.app/teacher/push/${select?._id}/subject/subject`,{classes:name})
+                .then((res)=> {alert(`${select?.name}  is assigned subject ${name}`);setAuto((prev)=>!prev);setShow(true)})
+                  .catch((err)=> console.log(err))
+    
+    }
 
     const handleAssign1 = (name) => {
       axios.put(`https://database-api-eight.vercel.app/student/${select1?._id}`,{class:name})
@@ -78,6 +95,7 @@ const Classes = ({id}) => {
                     <button className='click1' onClick={()=>handleCreate()}>Create</button>
                  </div>
                  </div>
+                 
 
                  <div>
                    {show?(
@@ -101,6 +119,51 @@ const Classes = ({id}) => {
                 </div>
                 )}
               </div>
+              <div>
+                   {show?(
+                    <div className='white2'> 
+                      <h5>Assign Class to Teacher</h5>
+                      <div className="dropDown">
+                   {name1?.map((name,index)=>{return(
+                    <div onClick={()=>handleId(name)} key={index}>{name.name}</div>
+
+                   )})} 
+                  </div>
+                  </div>
+                  ):(
+                  <div className='white2'>
+                      <h5> Select Class</h5>
+                      <div className="dropDown">
+                       {name?.classes?.map((name,index)=>{return(
+                             <div onClick={()=>handleAssign3(name.key)} key={index}>{name.key}</div>
+                      )})} 
+                </div>
+                </div>
+                )}
+              </div>
+              <div>
+                   {show?(
+                    <div className='white2'> 
+                      <h5>Assign subject to Teacher</h5>
+                      <div className="dropDown">
+                   {name1?.map((name,index)=>{return(
+                    <div onClick={()=>handleId(name)} key={index}>{name.name}</div>
+
+                   )})} 
+                  </div>
+                  </div>
+                  ):(
+                  <div className='white2'>
+                      <h5> Select subject</h5>
+                      <div className="dropDown">
+                       {name?.classes?.map((name,index)=>{return(
+                             <div onClick={()=>handleAssign4(name.key)} key={index}>{name.key}</div>
+                      )})} 
+                </div>
+                </div>
+                )}
+              </div>
+
 
               <div>
                     {show1?(
--- src/components/teacher.jsx
+++ src/components/teacher.jsx
@@ -8,8 +8,10 @@ import {useParams,useNavigate } from "react-router-dom"
 const Teacher = ({setBack}) => {
   const [names,setNames] = useState([])
   const [toggle, setToggle]=useState()
+  const [select, setSelect]=useState('')
+  const [select2, setSelect2]=useState('')
   const [name,setName] = useState('')
-  const [staff,setStaff] = useState('')
+  const [teacher,setTeacher] = useState('')
   const [next,setNext] = useState(0)
   const [change,setChange] = useState(false)
   const [show,setShow] = useState(true)
@@ -17,12 +19,12 @@ const Teacher = ({setBack}) => {
   const {id} = useParams()
   const nav = useNavigate()
 
-  const items =["a","b","c"]
+  
   
   useEffect(() => {
  
-    axios.get(`https://database-api-eight.vercel.app/staff/${id}`)
-              .then((res)=> {console.log(res.data);setStaff(res.data)})
+    axios.get(`https://database-api-eight.vercel.app/teacher/${id}`)
+              .then((res)=> {console.log(res.data);setTeacher(res.data)})
               .catch((err)=> console.log(err))
   
               axios.get(`https://database-api-eight.vercel.app/student`)
@@ -41,41 +43,41 @@ const Teacher = ({setBack}) => {
         <div className='click2' onClick={()=>nav(-1)}><FaArrowCircleLeft /></div>
         </div>
          <div className='two'>
-         <div style={{marginTop:"20px"}}> {staff? <img className='img' src={staff?.image} />
+         <div style={{marginTop:"20px"}}> {teacher? <img className='img' src={teacher?.image} />
                          :
                          <FaUser className='img'/>}</div>
          <div  className='three2 icons'>
-                <div onClick={()=>nav(`/chat/staff/${id}`)}>Notice</div>
-                <div onClick={()=>nav(`/studentActivity/${staff._id}`)}>Activity</div>
-                <div onClick={()=>nav(`/question/${staff._id}`)}>Questions </div>
-                <div onClick={()=>nav(`/profile/staff/${staff._id}`)}>profile</div>
+                <div onClick={()=>nav(`/chat/teacher/${id}`)}>Notice</div>
+                <div onClick={()=>nav(`/studentActivity/${teacher._id}`)}>Activity</div>
+                <div onClick={()=>nav(`/question/${teacher._id}`)}>Questions </div>
+                <div onClick={()=>nav(`/profile/teacher/${teacher._id}`)}>profile</div>
                 </div >
 
                 <div>{toggle?(
   <div className='bars'   onClick={()=>setToggle(pre => !pre)}><FaBars/></div>):(
     <div className="icons bars">
-      <div onClick={()=>nav(`/chat/staff/${id}`)}>Notice</div> 
-        <div onClick={()=>nav(`/activity/${staff._id}`)}>Activity</div>
-                <div onClick={()=>nav(`/question/${staff._id}`)}>Questions </div>
-                <div onClick={()=>nav(`/profile/staff/${staff._id}`)}>profile</div>
+      <div onClick={()=>nav(`/chat/teacher/${id}`)}>Notice</div> 
+        <div onClick={()=>nav(`/activity/${teacher._id}`)}>Activity</div>
+                <div onClick={()=>nav(`/question/${teacher._id}`)}>Questions </div>
+                <div onClick={()=>nav(`/profile/teacher/${teacher._id}`)}>profile</div>
                </div>)}
  </div>
          </div>
       <div>
       <div>
               <h4>
-                Key:{staff?.key} <br />
-                Name:{staff?.name} <br />
-                class:{staff?.class}
+                Key:{teacher?.key} <br />
+                Name:{teacher?.name} <br />
+                
               </h4>
               <div>Select Class: <select onChange={(e)=>setSelect(e.target.value)}> 
-                   {items.map((value,index)=>{return(
+                   {teacher?.class?.class?.map((value,index)=>{return(
                     <div key={index}><option value={value}>{value}</option></div>
                    )})}
                   </select>
               </div>
-              <div>Select Subject: <select onChange={(e)=>setSelect(e.target.value)}> 
-                {items.map((value,index)=>{return(
+              <div>Select Subject: <select onChange={(e)=>setSelect2(e.target.value)}> 
+                {teacher?.subject?.subject?.map((value,index)=>{return(
                     <div key={index}><option value={value}>{value}</option></div>
                    )})} </select>
               </div>
@@ -84,7 +86,7 @@ const Teacher = ({setBack}) => {
     </div>
     <div className='white'>
          <h2> STUDENT LIST</h2>
-         {names?.filter((item) => {if(item?.class === staff?.class) return item.name})
+         {names?.filter((item) => {if(item?.class === select) return item.name})
           .map((item,index)=>  <div key={index}>
          <div onClick={()=>nav(`/user/${item._id}`)}> {item.name} </div>
          </div>)}
