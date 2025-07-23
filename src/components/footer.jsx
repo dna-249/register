4926bedc17edc3e3112ed21404008e20ae36c0a6
nuraalhaji <danamonuraa@gmail.com>
2025-07-23 18:41:48 +0100

    commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -35,11 +35,10 @@ const sendEmail = async()=>{
             <h3>Email</h3>
              <div style={{border:"none",background:"transparent",height:"400px",position:"relative",top:"0px",alignContent:"baseline"}} className='signUp'>
             <div style={{borderRadius:"10px",padding:"20px"}}>
-            <div>:<input onChange={(e)=>setName(e.target.value)} type='text'/></div>   
-           <div>Message:<textarea onChange={(e)=>setMessage(e.target.value)} style={{outlineColor:"lightgreen",border:" 1px solid rgba(128, 127, 127, 0.28)"}} cols={30} rows={5}   type='text'/></div>
-             <div>Name:<input onChange={(e)=>setName(e.target.value)} type='text'/></div>   
-           <div>Phone:<input onChange={(e)=>setPhone(e.target.value)}  type='number'/></div>           <div>Email:<input onChange={(e)=>setEmail(e.target.value)}  type='email'/></div>
-
+            <div>Message:<textarea onChange={(e)=>setMessage(e.target.value)} style={{outlineColor:"lightgreen",border:" 1px solid rgba(128, 127, 127, 0.28)"}} cols={30} rows={5}   type='text'/></div>
+             <div>Name:<input onChange={(e)=>setName(e.target.value)} type='text'/></div>            
+             <div>Email:<input onChange={(e)=>setEmail(e.target.value)}  type='email'/></div>
+            <div>Phone:<input onChange={(e)=>setPhone(e.target.value)}  type='number'/></div>
      
               <div><button style={{border:"none",outline:"none"}} onClick={()=>sendEmail()}> Send</button></div> 
         </div>
