f16fa45437256e6901b8a82ec1c2cfc107a85e64
nuraalhaji <danamonuraa@gmail.com>
2025-07-23 18:35:29 +0100

    commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -20,13 +20,13 @@ const html =`<div className="center">
              </div>`
 
 const sendEmail = async()=>{
- await axios.post("https://database-api-eight.vercel.app/email/email",{
+ await axios.post("https://database-api-eight.vercel.app/email",{
   from:email,
   to:"danamonuraalhaji@gmail.com",
   subject:subject,
   html:html,
  })
- .then(()=>alert("sent successfully")).catch((err)=>alert(err.message))
+ .then((res)=>alert("sent successfully")).catch((err)=>alert(err.message))
 }
 
   return (           
@@ -35,8 +35,9 @@ const sendEmail = async()=>{
             <h3>Email</h3>
              <div style={{border:"none",background:"transparent",height:"400px",position:"relative",top:"0px",alignContent:"baseline"}} className='signUp'>
             <div style={{borderRadius:"10px",padding:"20px"}}>
+            <div>:<input onChange={(e)=>setName(e.target.value)} type='text'/></div>   
            <div>Message:<textarea onChange={(e)=>setMessage(e.target.value)} style={{outlineColor:"lightgreen",border:" 1px solid rgba(128, 127, 127, 0.28)"}} cols={30} rows={5}   type='text'/></div>
-          <div>Name:<input onChange={(e)=>setName(e.target.value)} type='text'/></div>   
+             <div>Name:<input onChange={(e)=>setName(e.target.value)} type='text'/></div>   
            <div>Phone:<input onChange={(e)=>setPhone(e.target.value)}  type='number'/></div>           <div>Email:<input onChange={(e)=>setEmail(e.target.value)}  type='email'/></div>
 
      
