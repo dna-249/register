5ec9c54a9772c502266e48cd9e49ef13afda0dc4
Your Name <you@example.com>
2025-08-22 07:50:53 -0700

    commit

--- src/components/payment.jsx
+++ src/components/payment.jsx
@@ -2,6 +2,7 @@
  import { useState } from 'react';
  import axios from "axios"
  import { useParams } from 'react-router-dom';
+ import useControl from './useControl';
   
 const payment = () => {
   const [name,setName]= useState("")
@@ -11,7 +12,8 @@ const payment = () => {
   const [phone,setPhone]= useState('')
   const [email,setEmail]= useState('')
   const {id} = useParams()
-   
+
+   const{error,control} = useControl(name,phone,email,select,select2)
   const makePayment = async()=>{
     await axios.put("https://database-api-eight.vercel.app/pay/put",
       {email:email,
@@ -32,11 +34,11 @@ const payment = () => {
 <div style={{height:"fit-content",padding:"20px"}} className='signUp'>
 
     <div style={{borderRadius:"10px",padding:"5px",border:" 1px solid rgba(128, 127, 127, 0.28)"}}>
-    <div style={{marginTop:"-10px"}}><img src="/bg_eoa.jpg" width={300} height={300} alt="" />  <br />
+    <div ><img src="/bg_eoa.jpg" width={300} height={300} alt="" />  <br />
        </div>
           <h2> PROGRAM DESCRIPTION </h2>
           <div style={{fontSize:"20px", color:"coral",fontWeight:"bold"}}>Program Type  </div>
-          <p style={{fontSize:"20px",fontWeight:"bold"}}> {id}</p>
+          <p className="box" style={{fontSize:"20px",fontWeight:"bold"}}> {id}</p>
 
           <div style={{fontSize:"20px", color:"coral",fontWeight:"bold"}}>Select Time:   
             <select onChange={(e)=>setSelect2(e.target.value)}> 
@@ -46,7 +48,7 @@ const payment = () => {
       
     </select>
     </div>
-       <p style={{fontSize:"20px",fontWeight:"bold"}}> {select2}</p>
+       <p className="box" style={{fontSize:"20px",fontWeight:"bold"}}> {select2}</p>
           <div style={{fontSize:"20px", color:"coral",fontWeight:"bold"}}>Select Duration:  
             <select onChange={(e)=>setSelect(e.target.value)}> 
       <option value="50000"> One Month Program</option>
@@ -55,21 +57,22 @@ const payment = () => {
       
     </select>
       </div>
-      {select  === '50000' && <p style={{fontSize:"20px",fontWeight:"bold"}}>One Month Program  </p> 
-    || select === '100000' && <p style={{fontSize:"20px",fontWeight:"bold"}}>Two Months  Program  </p>
-     || select === '150000' && <p style={{fontSize:"20px",fontWeight:"bold"}}>Three Months Program   </p>}
+      {select  === '50000' && <p className="box" style={{fontSize:"20px",fontWeight:"bold"}}>One Month Program  </p> 
+    || select === '100000' && <p className="box" style={{fontSize:"20px",fontWeight:"bold"}}>Two Months  Program  </p>
+     || select === '150000' && <p className="box" style={{fontSize:"20px",fontWeight:"bold"}}>Three Months Program   </p>}
        
     
        <div style={{fontSize:"20px", color:"coral",fontWeight:"bold"}}>Program Fees</div>
-        {select  === '50000' && <p style={{fontSize:"20px",fontWeight:"bold"}}>Fees: N50,000:00</p> 
-    || select === '100000' && <p style={{fontSize:"20px",fontWeight:"bold"}}>Fees: N100,000:00</p>
-     || select === '150000' && <p style={{fontSize:"20px",fontWeight:"bold"}}> Fees:N150,000</p>}
+        {select  === '50000' && <p className="box" style={{fontSize:"20px",fontWeight:"bold"}}>Fees: N50,000:00</p> 
+    || select === '100000' && <p className="box" style={{fontSize:"20px",fontWeight:"bold"}}>Fees: N100,000:00</p>
+     || select === '150000' && <p className="box" style={{fontSize:"20px",fontWeight:"bold"}}> Fees:N150,000</p>}
        
      <h3 style={{color:"green",fontWeight:"bolder"}}>  Fill the following information:</h3>
          <div>Full Name:<input onChange={(e)=>setName(e.target.value)}     type='text'/></div>
          <div>Phone:<input onChange={(e)=>setPhone(e.target.value)}     type='number'/></div>
          <div>Email:<input onChange={(e)=>setEmail(e.target.value)}     type='email'/></div>
-         <button onClick={() =>makePayment()}> Make Payment</button>
+         <h3 className='red'>{error}</h3>
+         <button style={{marginBottom:"20px"}} onClick={() =>control(makePayment)}> Make Payment</button>
     </div>
     </div>
   
